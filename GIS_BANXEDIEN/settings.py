5c46d5d2332b861b398f3a72b7759875dd028ce7
Minh2395 <vongominh2395@gmail.com>
2026-02-05 00:26:51 +0700

    Update settings.py (local config)

--- GIS_BANXEDIEN/settings.py
+++ GIS_BANXEDIEN/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "GIS_BANXEDIEN",
         "USER": "postgres",
-        "PASSWORD": "hoan2606",
+        "PASSWORD": "1234", # chỉnh lại mật khẩu là được
         "HOST": "localhost",
         "PORT": "5432",
     }
