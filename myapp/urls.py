9fe42fd1f42506ba56c053ebf90221f25a3fa582
Minh2395 <vongominh2395@gmail.com>
2026-02-05 10:28:23 +0700

    Select by product, product information, map

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,19 +1,12 @@
 from django.urls import path
-from myapp.views import auth_views, home_views
+from myapp.views import auth_views, home_views, map_views, product_detail_views
 
 urlpatterns = [
-    # Trang đăng nhập
     path('login/', auth_views.login_view, name='login'),
-    
-    # Trang đăng xuất
     path('logout/', auth_views.logout_view, name='logout'),
-    
-    # Trang chủ - SỬA DÒNG NÀY
-    path('', home_views.home, name='home'),  # Đổi từ auth_views.home_view thành home_views.home
-    
-    # Trang đăng ký
+    path('', home_views.home, name='home'),
     path('register/', auth_views.register_view, name='register'),
-    
-    # Trang admin
-    path('admin/dashboard/', auth_views.admin_dashboard_view, name='admin_dashboard')
+    path('admin/dashboard/', auth_views.admin_dashboard_view, name='admin_dashboard'),
+    path("map/", map_views.map_view, name="map"),
+    path("product/<int:product_id>/", product_detail_views.product_detail, name="product_detail")
 ]
