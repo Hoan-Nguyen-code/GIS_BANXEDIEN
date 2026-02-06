b7fc4693fdbdcba6351ef9bcc1a4f58fce283d86
HuyPhan1503 <phanhuy150305@gmail.com>
2026-02-06 08:08:28 +0700

    Thêm trang admin

--- GIS_BANXEDIEN/settings.py
+++ GIS_BANXEDIEN/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "GIS_BANXEDIEN",
         "USER": "postgres",
-        "PASSWORD": "1234", # chỉnh lại mật khẩu là được
+        "PASSWORD": "123456", # chỉnh lại mật khẩu là được
         "HOST": "localhost",
         "PORT": "5432",
     }
